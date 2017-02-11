6795160463d1a2cb5526d24760c5fd701718e1c2
FedorArbuzovHSE <ArbuzovFP@yandex.ru>
2017-02-12 01:27:58 +0300

    MessageMain

--- src/Datatron.java
+++ src/Datatron.java
@@ -1,4 +1,5 @@
 import org.telegram.telegrambots.api.methods.send.SendMessage;
+import org.telegram.telegrambots.api.objects.Audio;
 import org.telegram.telegrambots.api.objects.Update;
 import org.telegram.telegrambots.api.objects.replykeyboard.InlineKeyboardMarkup;
 import org.telegram.telegrambots.api.objects.replykeyboard.ReplyKeyboardMarkup;
@@ -15,32 +16,63 @@ class Datatron extends TelegramLongPollingBot {
 
     @Override
     public void onUpdateReceived(Update update) {
-        //System.out.println(update);
 
-        if(update.hasCallbackQuery()){
-            System.out.println(update.getCallbackQuery().getData());
+        Long userId = null;
+        String messageText = null;
+        String callbackData = null;
+        Audio audio = null;
+
+        if(update.hasMessage() && update.getMessage().hasText()) {
+            messageText = update.getMessage().getText();
+            userId = update.getMessage().getChatId();
         }
 
+        if(update.hasCallbackQuery()){
+            callbackData = update.getCallbackQuery().getData();
+            userId = update.getCallbackQuery().getMessage().getChatId();
+        }
 
-        // get text from bot
-        if (update.hasMessage() && update.getMessage().hasText()) {
+        // TODO: create audio hendler
+        if(update.getMessage().getAudio() != null){
+            audio = update.getMessage().getAudio();
+            userId = update.getMessage().getChatId();
+        }
 
-            String messageText = update.getMessage().getText();
-
-            if(messageText.startsWith("/start")) {
-                SendMessage message = new SendMessage() // Create a SendMessage object with mandatory fields
-                        .setChatId(update.getMessage().getChatId())
-                        .setText(Constants.START_MSG);
-                try {
-                    sendMessage(message); // Call method to send the message
-                    } catch (TelegramApiException e) {
-                    e.printStackTrace();
-                }
-            }
-
-            if(messageText.startsWith("/help")){
-
-
+        MessageMain messageFromUser = new MessageMain(userId, messageText, callbackData, audio);
+//        //System.out.println(update);
+//
+//        if(update.hasCallbackQuery()){
+//            System.out.println(update.getCallbackQuery().getData());
+//                SendMessage message = new SendMessage() // Create a SendMessage object with mandatory fields
+//                        .setChatId(update.getCallbackQuery().getMessage().getChatId())
+//                        .setText(update.getCallbackQuery().getData());
+//                try {
+//                    sendMessage(message); // Call method to send the message
+//                } catch (TelegramApiException e) {
+//                    e.printStackTrace();
+//                }
+//        }
+//
+//
+//        // get text from bot
+//        if (update.hasMessage() && update.getMessage().hasText()) {
+//
+//            String messageText = update.getMessage().getText();
+//
+//            if(messageText.startsWith("/start")) {
+//                SendMessage message = new SendMessage() // Create a SendMessage object with mandatory fields
+//                        .setChatId(update.getMessage().getChatId())
+//                        .setText(Constants.START_MSG);
+//                try {
+//                    sendMessage(message); // Call method to send the message
+//                    } catch (TelegramApiException e) {
+//                    e.printStackTrace();
+//                }
+//            }
+//
+//            if(messageText.startsWith("/help")){
+//
+//
 //                SendMessage message = new SendMessage();
 //                message.setText("First message without command");
 //                message.setChatId(update.getMessage().getChatId());
@@ -49,7 +81,9 @@ class Datatron extends TelegramLongPollingBot {
 //                List<InlineKeyboardButton> row = new ArrayList<>();
 //                InlineKeyboardButton button = new InlineKeyboardButton();
 //                button.setText("Edit message");
-//                button.setCallbackData("EDIT");
+//                //button.setCallbackData("");
+//                button.setUrl("https://youtu.be/swok2pcFtNI");
+//
 //                row.add(button);
 //                keyboard.add(row);
 //                markup.setKeyboard(keyboard);
@@ -59,67 +93,67 @@ class Datatron extends TelegramLongPollingBot {
 //                } catch (Throwable e) {
 //                    e.printStackTrace();
 //                }
-                SendMessage message = new SendMessage() // Create a SendMessage object with mandatory fields
-                        .setChatId(update.getMessage().getChatId())
-                        .setText(Constants.HELP_MSG);
-                try {
-                    sendMessage(message); // Call method to send the message
-                    } catch (TelegramApiException e) {
-                    e.printStackTrace();
-                }
-
-            }
-
-            if(messageText.startsWith("/search")){
-                if(messageText.endsWith("/search")){
-
-
-                }
-                else{
-                    //TODO: top machine learning algorithm
-                }
-            }
-        }
-    }
-
-    public void sendCustomKeyboard(String chatId) {
-        SendMessage message = new SendMessage();
-        message.setChatId(chatId);
-        message.setText("Custom message text");
-
-        // Create ReplyKeyboardMarkup object
-        InlineKeyboardMarkup keyboardMarkup = new InlineKeyboardMarkup();
-        // Create the keyboard (list of keyboard rows)
-        List<InlineKeyboardButton> keyboard = new ArrayList<>();
-        InlineKeyboardButton button = new InlineKeyboardButton();
-        // Create a keyboard row
-        KeyboardRow row = new KeyboardRow();
-        // Set each button, you can also use KeyboardButton objects if you need something else than text
-        row.add("Row 1 Button 1");
-        row.add("Row 1 Button 2");
-        row.add("Row 1 Button 3");
-        // Add the first row to the keyboard
-
-        // Create another keyboard row
-        row = new KeyboardRow();
-        // Set each button for the second line
-        row.add("Row 2 Button 1");
-        row.add("Row 2 Button 2");
-        row.add("Row 2 Button 3");
-        // Add the second row to the keyboard
-        keyboard.add(button);
-        // Set the keyboard to the markup
-        List<List<InlineKeyboardButton>> kb1 = new ArrayList<>();
-        keyboardMarkup.setKeyboard(kb1);
-        // Add it to the message
-        message.setReplyMarkup(keyboardMarkup);
-
-        try {
-            // Send the message
-            sendMessage(message);
-        } catch (TelegramApiException e) {
-            e.printStackTrace();
-        }
+////                SendMessage message = new SendMessage() // Create a SendMessage object with mandatory fields
+////                        .setChatId(update.getMessage().getChatId())
+////                        .setText(Constants.HELP_MSG);
+////                try {
+////                    sendMessage(message); // Call method to send the message
+////                    } catch (TelegramApiException e) {
+////                    e.printStackTrace();
+////                }
+//
+//            }
+//
+//            if(messageText.startsWith("/search")){
+//                if(messageText.endsWith("/search")){
+//
+//
+//                }
+//                else{
+//                    //TODO: top machine learning algorithm
+//                }
+//            }
+//        }
+//    }
+//
+//    public void sendCustomKeyboard(String chatId) {
+//        SendMessage message = new SendMessage();
+//        message.setChatId(chatId);
+//        message.setText("Custom message text");
+//
+//        // Create ReplyKeyboardMarkup object
+//        InlineKeyboardMarkup keyboardMarkup = new InlineKeyboardMarkup();
+//        // Create the keyboard (list of keyboard rows)
+//        List<InlineKeyboardButton> keyboard = new ArrayList<>();
+//        InlineKeyboardButton button = new InlineKeyboardButton();
+//        // Create a keyboard row
+//        KeyboardRow row = new KeyboardRow();
+//        // Set each button, you can also use KeyboardButton objects if you need something else than text
+//        row.add("Row 1 Button 1");
+//        row.add("Row 1 Button 2");
+//        row.add("Row 1 Button 3");
+//        // Add the first row to the keyboard
+//
+//        // Create another keyboard row
+//        row = new KeyboardRow();
+//        // Set each button for the second line
+//        row.add("Row 2 Button 1");
+//        row.add("Row 2 Button 2");
+//        row.add("Row 2 Button 3");
+//        // Add the second row to the keyboard
+//        keyboard.add(button);
+//        // Set the keyboard to the markup
+//        List<List<InlineKeyboardButton>> kb1 = new ArrayList<>();
+//        keyboardMarkup.setKeyboard(kb1);
+//        // Add it to the message
+//        message.setReplyMarkup(keyboardMarkup);
+//
+//        try {
+//            // Send the message
+//            sendMessage(message);
+//        } catch (TelegramApiException e) {
+//            e.printStackTrace();
+//        }
     }
 
     @Override
