37dfc4ef5fff7b5790ab08778c4a34417f6809d8
FedorArbuzovHSE <ArbuzovFP@yandex.ru>
2017-02-12 16:57:54 +0300

    MessageMain

---
+++ src/answerPackage/ListButtonTelegram.java
@@ -0,0 +1,13 @@
+package answerPackage;
+
+/**
+ * Created by fedor on 12.02.17.
+ */
+public final class ListButtonTelegram {
+
+    // TODO: Кирилл, сделай текст и колбек для всех кнопок
+
+    // Пример
+    // Text: Доход; Callback: D; Url: null
+
+}
