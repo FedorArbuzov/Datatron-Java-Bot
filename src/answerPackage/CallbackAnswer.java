2bb82bd48231749f8d0f139b69a1f3391563ea1a
FedorArbuzovHSE <ArbuzovFP@yandex.ru>
2017-02-17 20:32:05 +0300

    Datatron-Java-Bot

--- src/answerPackage/CallbackAnswer.java
+++ src/answerPackage/CallbackAnswer.java
@@ -5,6 +5,32 @@ package answerPackage;
  */
 public class CallbackAnswer {
 
+    // Text: Доходы; Callback: Revenues; Url: null
+    // Text: Расходы; Callback: Spendings; Url: null
+    // Text: Дефицит; Callback: Deficit; Url: null
+
+    // Text: Налоговые; Callback: TaxMoney; Url: null
+    // Text: Неналоговые; Callback: NonTaxMoney; Url: null
+
+    // Text: Плановые; Callback: Planned; Url: null
+    // Text: Фактические; Callback: Fact; Url: null
+    // Text: Текущие; Callback: Current; Url: null
+
+    // Text: Национальная оборона; Callback: NationalDefence; Url: null
+    // Text: Национальная безопасность; Callback: NationalSecurity; Url: null
+    // Text: Национальная экономика; Callback: NationalEconomics; Url: null
+    // Text: Образование; Callback: Education; Url: null
+    // Text: ЖКХ; Callback: HSC; Url: null
+    // Text: Культура; Callback: Culture; Url: null
+    // Text: Спорт; Callback: Sports; Url: null
+    // Text: Социальная политика; Callback: SocialPolicy; Url: null
+    // Text: Защита окружающей среды; Callback: EnvironmentProtection; Url: null
+    // Text: Общие расходы; Callback: OtherSpendings; Url: null
+
+    // Text: Руководство пользователя; Callback: FullDocumentation; Url: null
+    // Text: Смотреть ролик; Callback: WatchMovie; Url: https://www.youtube.com/watch?v=swok2pcFtNI
+    // Text: Оценить; Callback: Rate; Url: https://telegram.me/storebot?start=datatron_bot
+
     private String callbackAnswer;
 
     public CallbackAnswer(String _callbackAnswer){
@@ -15,9 +41,13 @@ public class CallbackAnswer {
 
     public Answer getAnswer(){
 
+        Answer answer;
+        switch (callbackAnswer){
+            case "Revenues":case "Spendings":case "Deficit":
+        }
      //   CallbackAnswer callback = CallbackAnswer(callbackAnswer);
      //   return callback.getAnswer();
-        Answer answer = new Answer("Вы нажали на кнопку!", null);
+        answer = new Answer("Вы нажали на кнопку!", null);
         return answer;
     }
 }
