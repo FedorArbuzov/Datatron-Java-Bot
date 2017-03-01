b9f54e51e8dfba547ff8de8f70492cc0cee22588
FedorArbuzovHSE <ArbuzovFP@yandex.ru>
2017-03-02 00:21:33 +0300

    Datatron-Java-Bot

--- src/answerPackage/CallbackAnswer.java
+++ src/answerPackage/CallbackAnswer.java
@@ -56,6 +56,8 @@ public class CallbackAnswer {
         switch (callbackAnswer){
 
 
+            // FIRST VARIANT
+
             // Text: Доходы; Callback: Revenues; Url: null
             // Text: Расходы; Callback: Spendings; Url: null
             // Text: Дефицит; Callback: Deficit; Url: null
@@ -141,7 +143,26 @@ public class CallbackAnswer {
                 answer = new Answer(_textAnswer, matrixButton);
                 return answer;
 
-            case "Deficit":
+            // Text: Все; Callback: TNTMoney; Url: null
+            // Text: Налоговые; Callback: TaxMoney; Url: null
+            // Text: Неналоговые; Callback: NonTaxMoney; Url: null
+            // Text: Общегосударственные вопросы; Callback: GovernmentQ ; Url: null
+            // Text: Национальная оборона; Callback: NationalDefence; Url: null
+            // Text: Национальная безопасность; Callback: NationalSecurity; Url: null
+            // Text: Национальная экономика; Callback: NationalEconomics; Url: null
+            // Text: Образование; Callback: Education; Url: null
+            // Text: ЖКХ; Callback: HSC; Url: null
+            // Text: Культура; Callback: Culture; Url: null
+            // Text: Спорт; Callback: Sports; Url: null
+            // Text: Социальная политика; Callback: SocialPolicy; Url: null
+            // Text: Здравоохранение; Callback: Medicine; Url: null
+            // Text: Защита окружающей среды; Callback: EnvironmentProtection; Url: null
+            // Text: Общие расходы; Callback: OtherSpendings; Url: null
+
+            case "Deficit":case "TNTMoney":case "TaxMoney":case "NonTaxMoney":case "GovernmentQ":
+            case "NationalDefence":case "NationalSecurity":case "NationalEconomics":case "Education":
+            case "HSC":case "Culture":case "Sports":case "SocialPolicy":case "Medicine":
+            case "EnvironmentProtection":case "OtherSpendings":
                 _textAnswer = "Выберите тип:";
 
                 // Text: Плановые; Callback: Planned; Url: null
@@ -165,9 +186,72 @@ public class CallbackAnswer {
                 answer = new Answer(_textAnswer, matrixButton);
                 return answer;
 
+            case "Planned":case "Fact":
+                _textAnswer = "Данные какого года вас интересуют?";
+
+                matrixButton = new ArrayList<>();
+
+                buttonRow = new ArrayList<>();
+                buttonRow.add(new ButtonTelegram("2007", "2007", null));
+                buttonRow.add(new ButtonTelegram("2008", "2008", null));
+                buttonRow.add(new ButtonTelegram("2009", "2009", null));
+                matrixButton.add(buttonRow);
+
+                buttonRow = new ArrayList<>();
+                buttonRow.add(new ButtonTelegram("2010", "2010", null));
+                buttonRow.add(new ButtonTelegram("2011", "2011", null));
+                buttonRow.add(new ButtonTelegram("2012", "2012", null));
+                matrixButton.add(buttonRow);
+
+                buttonRow = new ArrayList<>();
+                buttonRow.add(new ButtonTelegram("2013", "2007", null));
+                buttonRow.add(new ButtonTelegram("2014", "2008", null));
+                buttonRow.add(new ButtonTelegram("2015", "2009", null));
+                matrixButton.add(buttonRow);
+
+                buttonRow = new ArrayList<>();
+                buttonRow.add(new ButtonTelegram("2016", "2016", null));
+                buttonRow.add(new ButtonTelegram("2017", "2017", null));
+                matrixButton.add(buttonRow);
+
+                answer = new Answer(_textAnswer, matrixButton);
+                return answer;
+
+            case "2007":case "2008":case "2009":case "2010":case "2011":
+            case "2012":case "2013":case "2014":case "2015":case "2016":
+            case "2017":case "Current":
+
+                _textAnswer = "Выберите территорию:";
+
+                matrixButton = new ArrayList<>();
+
+                buttonRow = new ArrayList<>();
+                buttonRow.add(new ButtonTelegram("Федеральный бюджет", "FederalBudget", null));
+                buttonRow.add(new ButtonTelegram("Россия", "Russia", null));
+                matrixButton.add(buttonRow);
+
+                buttonRow = new ArrayList<>();
+                buttonRow.add(new ButtonTelegram("Москва", "Moscow", null));
+                buttonRow.add(new ButtonTelegram("Московская область", "MoscowState", null));
+                matrixButton.add(buttonRow);
+
+                buttonRow = new ArrayList<>();
+                buttonRow.add(new ButtonTelegram("Другие", "Other", null));
+                matrixButton.add(buttonRow);
+
+                answer = new Answer(_textAnswer, matrixButton);
+                return answer;
+
+            case "Other":
+                _textAnswer = "Чтобы узнать информацию о других субъектах России введите, /cr *название региона* (например, /cr Рязанская область)";
+
+
+                answer = new Answer(_textAnswer, null);
+                return answer;
+
         }
 
-        answer = new Answer("Вы нажали на кнопку!", null);
+        answer = new Answer("НЕ ЛЕЗЬ !!!!", null);
         return answer;
     }
 }
