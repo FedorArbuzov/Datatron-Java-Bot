ec0c471d7285aef63af9ce6802df04fa6430e6b7
FedorArbuzovHSE <ArbuzovFP@yandex.ru>
2017-03-06 11:51:27 +0300

    Datatron-Java-Bot

--- src/Main.java
+++ src/Main.java
@@ -2,17 +2,39 @@ import org.telegram.telegrambots.ApiContextInitializer;
 import org.telegram.telegrambots.TelegramBotsApi;
 import org.telegram.telegrambots.exceptions.TelegramApiException;
 
+import java.net.InetAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class Main {
 
     public static void main(String[] args) {
+
+
         ApiContextInitializer.init();
 
         TelegramBotsApi botsApi = new TelegramBotsApi();
 
         try {
             botsApi.registerBot(new Datatron());
+            mainWeb();
         } catch (TelegramApiException e) {
             e.printStackTrace();
+        }
+        catch (Exception ex){
+            ex.printStackTrace();
+        }
+    }
+
+
+    public static void mainWeb() throws Exception {
+
+        ServerSocket Server = new ServerSocket (5000, 10, InetAddress.getByName("127.0.0.1"));
+        System.out.println ("TCPServer Waiting for client on port 5000");
+
+        while(true) {
+            Socket connected = Server.accept();
+            (new JavaServer(connected)).start();
         }
     }
 }
